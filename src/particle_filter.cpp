13c53edfb695b1d7e64953aba709f8d2fe47370a
Anjana Rajam <anjanarsid@gmail.com>
2020-06-29 18:41:01 +0200

    bug fix

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -189,7 +189,7 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
     /* Loop through every particle */
     for (auto& particle : particles_) {
         /* Initialize the observed measurements for every particle */
-        double x_c{}, y_c{};       
+        const double x_c{}, y_c{};       
 
         /* Get the x and y co-ordinates of the particle */
         x_p = particle.x;
@@ -202,7 +202,7 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////
         /* Store the transformed co-ordinates */
         std::transform(observations.begin(), observations.end(), transformed_cordinates.begin(),
-            [&](LandmarkObs& obs_meas) {
+            [&](const LandmarkObs& obs_meas) {
                 /* Get the x and y co-ordinates of the observed measurements */
                 x_c = obs_meas.x;
                 y_c = obs_meas.y;
